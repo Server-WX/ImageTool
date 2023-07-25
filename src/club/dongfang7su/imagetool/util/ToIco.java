ef1873c9b57a97cf932111a01232c43def96a34c
Server-WX <lfl3518963655@163.com>
2023-07-26 03:11:13 +0800

    设定转换大小为256x256

--- src/club/dongfang7su/imagetool/util/ToIco.java
+++ src/club/dongfang7su/imagetool/util/ToIco.java
@@ -16,7 +16,7 @@ public class ToIco {
             BufferedImage image = ImageIO.read(new File(filePath));
 
             // Resize image to 256x256
-            BufferedImage resizedImage = resizeImage(image, 256, 256);
+            BufferedImage resizedImage = resizeImage(image);
 
             // Create a list to hold different size images for ICO
             List<BufferedImage> imageList = new ArrayList<>();
@@ -29,9 +29,9 @@ public class ToIco {
         }
     }
 
-    private static BufferedImage resizeImage(BufferedImage originalImage, int width, int height) {
-        BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        resizedImage.getGraphics().drawImage(originalImage.getScaledInstance(width, height, BufferedImage.SCALE_SMOOTH), 0, 0, null);
+    private static BufferedImage resizeImage(BufferedImage originalImage) {
+        BufferedImage resizedImage = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
+        resizedImage.getGraphics().drawImage(originalImage.getScaledInstance(256, 256, BufferedImage.SCALE_SMOOTH), 0, 0, null);
         return resizedImage;
     }
 
